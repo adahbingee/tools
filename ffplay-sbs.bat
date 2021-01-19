8e65f9865adc37f7e1d7cb27167cb51f93df5b03
songbingyi <songbingyi@bigo.sg>
2021-01-19 17:31:41 +0800

    update

--- ffplay-sbs.bat
+++ ffplay-sbs.bat
@@ -1,3 +1,4 @@
-set left=left.mp4
-set right=right.mp4
-ffplay -loop 0 -f lavfi "movie=%left%:loop=0:discontinuity=1,scale=iw/2:ih[v0];movie=%right%:loop=0:discontinuity=1,scale=iw/2:ih[v1];[v0][v1]hstack"
+setlocal enabledelayedexpansion
+set left=%~nx1
+set right=%~nx2
+ffplay -window_title "%left% %right%" -loop 0 -f lavfi "movie=%left%:loop=0:discontinuity=1,scale=iw/2:ih[v0];movie=%right%:loop=0:discontinuity=1,scale=iw/2:ih[v1];[v0][v1]hstack"
