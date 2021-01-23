328b991df2c9b0aca2390cefcd8fb4812b4e5376
songbingyi <songbingyi@bigo.sg>
2021-01-23 17:18:47 +0800

    update

--- ffmpeg-extract-key-frame.bat
+++ ffmpeg-extract-key-frame.bat
@@ -1,1 +1,4 @@
-ffmpeg -skip_frame nokey -i %1 -vsync 0 %04d.png
+:: skip_frame
+:: nointra  Discard all frames except I frames.
+:: nokey    Discard all frames excepts keyframes.
+ffmpeg -skip_frame nointra -i %1 -vsync 0 %%04d.png
