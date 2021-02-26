c412c9b73d9fa351a4c3848f188a69b6bb4d38c7
songbingyi <songbingyi@bigo.sg>
2021-02-26 12:09:28 +0800

    update

--- ffplay-sbs-different-resolution.bat
+++ ffplay-sbs-different-resolution.bat
@@ -4,4 +4,3 @@ set rit=%~nx2
 set cmd_lft=movie=%lft%:loop=0:discontinuity=1,scale=720:1280,drawtext=text='%lft%':fontcolor=white:box=1:boxcolor=black@0.5[v0]
 set cmd_rit=movie=%rit%:loop=0:discontinuity=1,scale=720:1280,drawtext=text='%rit%':fontcolor=white:box=1:boxcolor=black@0.5[v1]
 ffplay -window_title "%lft% %rit%" -loop 0 -f lavfi "%cmd_lft%;%cmd_rit%;[v0][v1]hstack=2"
-pause
--- ffplay-tri-different-resolution.bat
+++ ffplay-tri-different-resolution.bat
@@ -6,4 +6,3 @@ set cmd_lft=movie=%lft%:loop=0:discontinuity=1,scale=720:1280,drawtext=text='%lf
 set cmd_mid=movie=%mid%:loop=0:discontinuity=1,scale=720:1280,drawtext=text='%mid%':fontcolor=white:box=1:boxcolor=black@0.5[v1]
 set cmd_rit=movie=%rit%:loop=0:discontinuity=1,scale=720:1280,drawtext=text='%rit%':fontcolor=white:box=1:boxcolor=black@0.5[v2]
 ffplay -window_title "%lft% %mid% %rit%" -loop 0 -f lavfi "%cmd_lft%;%cmd_mid%;%cmd_rit%;[v0][v1][v2]hstack=3"
-pause
