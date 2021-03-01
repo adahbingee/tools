3b56d792e1d00788a797a32cf1ee9aafe2d53e0b
songbingyi <songbingyi@bigo.sg>
2021-03-01 09:56:10 +0800

    update

--- ffplay-sbs-different-resolution.bat
+++ ffplay-sbs-different-resolution.bat
@@ -1,6 +1,12 @@
 setlocal enabledelayedexpansion
+:: normalize size
+set szX=720
+set szY=1280
+:: resample algorithm
+set sws=lanczos
+:: inputs
 set lft=%~nx1
 set rit=%~nx2
-set cmd_lft=movie=%lft%:loop=0:discontinuity=1,scale=720:1280,drawtext=text='%lft%':fontcolor=white:box=1:boxcolor=black@0.5[v0]
-set cmd_rit=movie=%rit%:loop=0:discontinuity=1,scale=720:1280,drawtext=text='%rit%':fontcolor=white:box=1:boxcolor=black@0.5[v1]
+set cmd_lft=movie=%lft%:loop=0:discontinuity=1,scale=%szX%:%szY%:sws_flags=%sws%,drawtext=text='%lft%':fontcolor=white:box=1:boxcolor=black@0.5[v0]
+set cmd_rit=movie=%rit%:loop=0:discontinuity=1,scale=%szX%:%szY%:sws_flags=%sws%,drawtext=text='%rit%':fontcolor=white:box=1:boxcolor=black@0.5[v1]
 ffplay -window_title "%lft% %rit%" -loop 0 -f lavfi "%cmd_lft%;%cmd_rit%;[v0][v1]hstack=2"
