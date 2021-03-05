149e7338fd331d8a5f53b10acb391ea93d92ad8b
songbingyi <songbingyi@bigo.sg>
2021-03-05 16:28:18 +0800

    update

--- ffmpeg-convert-yuv420.bat
+++ ffmpeg-convert-yuv420.bat
@@ -1,3 +1,4 @@
 :: yuv420p  limited range 16-239
 :: yuvj420p full    range 0 -255
-ffmpeg -i %1 -c:v rawvideo -pix_fmt yuvj420p %~n1.yuv
+:: add -vsync 0 to drop duplicate frames
+ffmpeg -i %1 -c:v rawvideo -pix_fmt yuvj420p -vsync 0 %~n1.yuv
