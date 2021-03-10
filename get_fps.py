0ddc816d971e4c3f8fa56860da61d0b4db8c3ea7
songbingyi <songbingyi@bigo.sg>
2021-03-10 12:10:42 +0800

    update

---
+++ get_fps.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+import re
+
+path   = sys.argv[1]
+result = subprocess.run(['ffprobe', '-v', 'quiet', '-show_streams', '-select_streams', 'v:0', path], stdout=subprocess.PIPE)
+match  = re.search(r'avg_frame_rate=(\d+)\/(\d+)', result.stdout.decode('utf-8'))
+a      = match.group(1)
+b      = match.group(2)
+print('{}'.format(float(a)/float(b)))
