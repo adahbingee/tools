7479ff2210298148deae39ecc07a86590dd83b57
songbingyi <songbingyi@bigo.sg>
2021-03-11 11:35:42 +0800

    update

--- get_fps.py
+++ get_fps.py
@@ -1,11 +1,15 @@
-import subprocess
-import sys
 import re
+import sys
+import glob
+import subprocess
 
-path   = sys.argv[1]
-result = subprocess.run(['ffprobe', '-v', 'quiet', '-show_streams', '-select_streams', 'v:0', path], stdout=subprocess.PIPE)
-match  = re.search(r'avg_frame_rate=(\d+)\/(\d+)', result.stdout.decode('utf-8'))
-a      = match.group(1)
-b      = match.group(2)
-fps    = round(float(a)/float(b), 2)
-print('{}'.format(fps))
+fileList = glob.glob('./*.mp4')
+fileList = sorted(fileList)
+
+for path in fileList:
+    result = subprocess.run(['ffprobe', '-v', 'quiet', '-show_streams', '-select_streams', 'v:0', path], stdout=subprocess.PIPE)
+    match  = re.search(r'avg_frame_rate=(\d+)\/(\d+)', result.stdout.decode('utf-8'))
+    a      = match.group(1)
+    b      = match.group(2)
+    fps    = float(a)/float(b)
+    print('{} {:0>6.2f}'.format(path, fps))
