a85f08bb3b75a631fc23de86da4f54077c3ff020
bingyisong <bingyisong@tencent.com>
2021-09-29 21:42:50 +0800

    update

--- ffmpeg-screen-record.bat
+++ ffmpeg-screen-record.bat
@@ -1,2 +1,3 @@
 :: https://trac.ffmpeg.org/wiki/Capture/Desktop
+::ffmpeg -f gdigrab -framerate 30 -i desktop -c:v h264_nvenc -qp 0 output.mkv
 ffmpeg -f gdigrab -framerate 30 -i desktop -pix_fmt yuv420p -c:v libx265 output.mp4
